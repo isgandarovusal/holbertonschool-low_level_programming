d0e9a2d6374241246b4d5b3b4a711117140c804c
root <isgandarovusal@gmail.com>
2025-12-19 12:53:05 +0000

    Task 0: Write a program that prints its name

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - prints program name
+ * @argc: number of arguments
+ * @argv: array of arguments
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc; /* explicitly ignore unused parameter */
+	printf("%s\n", argv[0]);
+	return (0);
+}
