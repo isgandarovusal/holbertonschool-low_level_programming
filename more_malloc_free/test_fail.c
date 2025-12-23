6c176ad3e3d442e01d36c5d4ff7bedaf1905d87e
Isgandarov Usal <isgandarovusal@gmail.com>
2025-12-23 10:59:54 +0000

    Add more_malloc_free task 0: malloc_checked function

---
+++ more_malloc_free/test_fail.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+int main(void)
+{
+    void *ptr;
+    
+    /* Bu çox böyük bir ölçüdür, malloc fail olmalıdır */
+    ptr = malloc_checked((unsigned int)-1);
+    
+    /* Əgər bura çatıbsa, deməli malloc_checked səhv işləyir */
+    printf("Error: Should have exited with 98\n");
+    return (1);
+}
