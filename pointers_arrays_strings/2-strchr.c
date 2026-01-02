c4eb05b341c6802c3b1dafe21b613b020d228ba5
root <isgandarovusal@lgmail.com>
2026-01-02 16:27:13 +0000

    Task 2: strchr function (corrected)

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _strchr - Locates a character in a string
+ * @s: The string to search
+ * @c: The character to find
+ *
+ * Return: Pointer to first occurrence of c, or NULL if not found
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s != '\0')
+	{
+		if (*s == c)
+			return (s);
+		s++;
+	}
+	if (c == '\0')
+		return (s);
+	return (0);
+}
