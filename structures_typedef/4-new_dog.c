2dc5755c72202254081b5f72fe1359a636ce547a
root <isgandarovusal@lgmail.com>
2026-01-02 14:46:35 +0000

    Fix new_dog: replace string.h with custom functions

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,40 @@
 #include <stdlib.h>
-#include <string.h>
 #include "dog.h"
+
+/**
+ * _strlen - calculates length of string
+ * @s: string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (s[len] != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
+ * _strcpy - copies string
+ * @dest: destination
+ * @src: source
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
 
 /**
  * new_dog - creates a new dog
@@ -20,8 +54,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	if (new_dog == NULL)
 		return (NULL);
 
-	name_len = strlen(name);
-	owner_len = strlen(owner);
+	name_len = _strlen(name);
+	owner_len = _strlen(owner);
 
 	name_copy = malloc(sizeof(char) * (name_len + 1));
 	if (name_copy == NULL)
@@ -38,8 +72,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 		return (NULL);
 	}
 
-	strcpy(name_copy, name);
-	strcpy(owner_copy, owner);
+	_strcpy(name_copy, name);
+	_strcpy(owner_copy, owner);
 
 	new_dog->name = name_copy;
 	new_dog->age = age;
