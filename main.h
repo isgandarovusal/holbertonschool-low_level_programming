acac323d2735740e4cc294d191448c27948b310f
root <isgandarovusal@lgmail.com>
2026-01-02 16:24:49 +0000

    Task 0: memset function

--- main.h
+++ main.h
@@ -1,8 +1,6 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-char *create_array(unsigned int size, char c);
-char *_strdup(char *str);
-char *str_concat(char *s1, char *s2);
+char *_memset(char *s, char b, unsigned int n);
 
 #endif
