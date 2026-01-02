fd19a233b1e21d76de95d58a3c9104cb01cdf9ae
root <isgandarovusal@lgmail.com>
2026-01-02 15:04:37 +0000

    Fix: Add missing 0-reset_to_98.c file

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - updates the value it points to to 98
+ * @n: pointer to integer
+ *
+ * Description: Takes a pointer to an int and updates
+ * the value it points to to 98.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
