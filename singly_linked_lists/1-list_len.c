45a3187cc8b0aa845c181e5f02a44a3041a961a0
Isgandarov Vusal <isgandarovusal@gmail.com>
2026-01-13 10:26:18 +0000

    Fix: list_len takes pointer to pointer for checker compatibility

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -2,18 +2,23 @@
 
 /**
  * list_len - returns the number of elements in a linked list_t list
- * @h: pointer to the head of the list
+ * @h: pointer to pointer of the head of the list
  * Return: number of elements
  */
-size_t list_len(const list_t *h)
+size_t list_len(const list_t **h)
 {
     size_t count = 0;
-
-    while (h != NULL)
+    const list_t *current;
+    
+    if (h == NULL)
+        return (0);
+    
+    current = *h;
+    while (current != NULL)
     {
         count++;
-        h = h->next;
+        current = current->next;
     }
-
+    
     return (count);
 }
