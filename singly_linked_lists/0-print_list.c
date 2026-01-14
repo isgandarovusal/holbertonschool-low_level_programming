2684e942ab9d81b4b382aaa0aac3774291c18507
Vusal Isgandarov <isgandarovusal@gmail.com>
2026-01-14 10:09:27 +0000

    Task 0 and 1: Print list and List length

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -1,25 +1,27 @@
-#include "lists.h"
 #include <stdio.h>
+#include "lists.h"
 
 /**
- * print_list - prints all elements of a list_t list
+ * print_list - prints all the elements of a list_t list
  * @h: pointer to the head of the list
+ *
  * Return: the number of nodes
  */
 size_t print_list(const list_t *h)
 {
-	size_t count = 0;
+    size_t count = 0;
+    const list_t *current = h;
 
-	while (h != NULL)
-	{
-		if (h->str == NULL)
-			printf("[0] (nil)\n");
-		else
-			printf("[%u] %s\n", h->len, h->str);
+    while (current != NULL)
+    {
+        if (current->str == NULL)
+            printf("[0] (nil)\n");
+        else
+            printf("[%u] %s\n", current->len, current->str);
+        
+        count++;
+        current = current->next;
+    }
 
-		h = h->next;
-		count++;
-	}
-
-	return (count);
+    return (count);
 }
