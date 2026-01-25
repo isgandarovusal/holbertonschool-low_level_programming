413730b06b6cb6f9d61e0ddfb2d80fe7ba1fef8f
Vusal Isgandarov <isgandarovusal@gmail.com>
2026-01-25 10:41:54 +0000

    Task 3: Fix Betty style - reduce main function lines

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -16,72 +16,56 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, close_from, close_to;
-	ssize_t bytes_read, bytes_written;
-	char buffer[BUFFER_SIZE];
-	mode_t permissions = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+	int fd_from, fd_to;
+	ssize_t r, w;
+	char b[BUFFER_SIZE];
+	mode_t p = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
 
-	/* Check argument count */
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-
-	/* Open source file for reading */
 	fd_from = open(argv[1], O_RDONLY);
 	if (fd_from == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-
-	/* Open destination file for writing */
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, permissions);
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, p);
 	if (fd_to == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(2, "Error: Can't write to %s\n", argv[2]);
 		close(fd_from);
 		exit(99);
 	}
-
-	/* Copy content in chunks of BUFFER_SIZE */
-	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
+	while ((r = read(fd_from, b, BUFFER_SIZE)) > 0)
 	{
-		bytes_written = write(fd_to, buffer, bytes_read);
-		if (bytes_written != bytes_read)
+		w = write(fd_to, b, r);
+		if (w != r)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			dprintf(2, "Error: Can't write to %s\n", argv[2]);
 			close(fd_from);
 			close(fd_to);
 			exit(99);
 		}
 	}
-
-	/* Check if read failed */
-	if (bytes_read == -1)
+	if (r == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
 		close(fd_from);
 		close(fd_to);
 		exit(98);
 	}
-
-	/* Close file descriptors */
-	close_from = close(fd_from);
-	close_to = close(fd_to);
-
-	if (close_from == -1)
+	if (close(fd_from) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+		dprintf(2, "Error: Can't close fd %d\n", fd_from);
 		exit(100);
 	}
-
-	if (close_to == -1)
+	if (close(fd_to) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+		dprintf(2, "Error: Can't close fd %d\n", fd_to);
 		exit(100);
 	}
-
 	return (0);
 }
