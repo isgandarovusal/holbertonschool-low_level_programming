106693302060b356402d9272c682eb96779916a5
Vusal Isgandarov <isgandarovusal@gmail.com>
2026-01-25 10:52:48 +0000

    Task 3: Betty compliant with helper function

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -6,7 +6,36 @@
 #include <sys/stat.h>
 
 #define BUFFER_SIZE 1024
-#define P (S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH)
+
+/**
+ * print_error_and_exit - prints error and exits
+ * @fd: file descriptor for close errors
+ * @filename: filename for read/write errors
+ * @error_type: 1=read, 2=write, 3=close, 4=usage
+ * @exit_code: exit code to use
+ * @close_fd1: fd to close before exit (or -1)
+ * @close_fd2: fd to close before exit (or -1)
+ */
+void print_error_and_exit(int fd, char *filename,
+			  int error_type, int exit_code,
+			  int close_fd1, int close_fd2)
+{
+	if (close_fd1 != -1)
+		close(close_fd1);
+	if (close_fd2 != -1)
+		close(close_fd2);
+
+	if (error_type == 1)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", filename);
+	else if (error_type == 2)
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", filename);
+	else if (error_type == 3)
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+	else if (error_type == 4)
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+
+	exit(exit_code);
+}
 
 /**
  * main - copies the content of a file to another file
@@ -17,19 +46,37 @@
  */
 int main(int argc, char *argv[])
 {
-	int f1, f2; ssize_t r, w; char b[BUFFER_SIZE];
-	if (argc != 3) dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
-	f1 = open(argv[1], O_RDONLY);
-	if (f1 == -1) dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), exit(98);
-	f2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, P);
-	if (f2 == -1) dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), close(f1), exit(99);
-	while ((r = read(f1, b, BUFFER_SIZE)) > 0)
+	int fd_from, fd_to;
+	ssize_t bytes_read, bytes_written;
+	char buffer[BUFFER_SIZE];
+	mode_t perms = S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH;
+
+	if (argc != 3)
+		print_error_and_exit(0, NULL, 4, 97, -1, -1);
+
+	fd_from = open(argv[1], O_RDONLY);
+	if (fd_from == -1)
+		print_error_and_exit(0, argv[1], 1, 98, -1, -1);
+
+	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, perms);
+	if (fd_to == -1)
+		print_error_and_exit(0, argv[2], 2, 99, fd_from, -1);
+
+	while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0)
 	{
-		w = write(f2, b, r);
-		if (w != r) dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), close(f1), close(f2), exit(99);
+		bytes_written = write(fd_to, buffer, bytes_read);
+		if (bytes_written != bytes_read)
+			print_error_and_exit(0, argv[2], 2, 99, fd_from, fd_to);
 	}
-	if (r == -1) dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]), close(f1), close(f2), exit(98);
-	if (close(f1) == -1) dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f1), exit(100);
-	if (close(f2) == -1) dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", f2), exit(100);
+
+	if (bytes_read == -1)
+		print_error_and_exit(0, argv[1], 1, 98, fd_from, fd_to);
+
+	if (close(fd_from) == -1)
+		print_error_and_exit(fd_from, NULL, 3, 100, -1, -1);
+
+	if (close(fd_to) == -1)
+		print_error_and_exit(fd_to, NULL, 3, 100, -1, -1);
+
 	return (0);
 }
