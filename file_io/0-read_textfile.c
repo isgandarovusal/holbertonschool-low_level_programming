e08825c484e31fde05d7cbaffcf633fe77c5ed4d
Vusal Isgandarov <isgandarovusal@gmail.com>
2026-01-25 10:33:36 +0000

    Task 0: Create read_textfile function

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -18,35 +18,24 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t n_read, n_write;
 	char *buffer;
 
-	if (filename == NULL)
+	if (!filename)
 		return (0);
 
 	buffer = malloc(sizeof(char) * letters);
-	if (buffer == NULL)
+	if (!buffer)
 		return (0);
 
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
-	{
-		free(buffer);
-		return (0);
-	}
+		return (free(buffer), 0);
 
 	n_read = read(fd, buffer, letters);
 	if (n_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+		return (free(buffer), close(fd), 0);
 
 	n_write = write(STDOUT_FILENO, buffer, n_read);
 	if (n_write == -1 || n_write != n_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+		return (free(buffer), close(fd), 0);
 
 	free(buffer);
 	close(fd);
